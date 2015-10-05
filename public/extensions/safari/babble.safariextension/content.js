bae5f9c18cd7441058f713bb637701ab85f4d229
Venkat <venkat.pedapati@gmail.com>
2015-10-05 06:43:39 +0530

    Fixed multi-voice bug in Safari

--- public/extensions/safari/babble.safariextension/content.js
+++ public/extensions/safari/babble.safariextension/content.js
@@ -13,5 +13,7 @@ function handleMessage(msgEvent) {
     }
 }
 
-safari.self.addEventListener("message", handleMessage, false);
+if(window.top == window) {
+    safari.self.addEventListener("message", handleMessage, false);
+}
 
